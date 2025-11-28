a1b46c25ded16cbc1bd8438fd995d6410630bae1
uwarajm <uwarajmohan@gmail.com>
2025-11-28 20:49:50 +0530

    FunctionScope

---
+++ functionscope.js
@@ -0,0 +1,39 @@
+function login()
+{
+  console.log("Enter credentials to login");
+  
+}
+login()
+
+function minus(a,b)
+{
+let c=a-b
+console.log("answer is", c);
+  
+}
+minus("1500", "1000")
+
+function multiply(a,b)
+{
+let c=a*b
+return c
+  
+}
+console.log(multiply("1500", "1000"))
+
+function register()
+{
+  let fname = "uwaraj"
+  let lname = "M"
+  
+  console.log(" Firstname", fname);
+
+  if(true)
+  {
+    console.log(" Lastname", lname);
+    
+  }
+  
+}
+//console.log("submit the page", yourname);
+register() 
