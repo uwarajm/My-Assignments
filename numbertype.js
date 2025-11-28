fa21e467b5cb71c354fd2fe431879ad4fef5fe4b
uwarajm <uwarajmohan@gmail.com>
2025-11-28 20:51:53 +0530

    NumberType
    positive and negative number

---
+++ numbertype.js
@@ -0,0 +1,22 @@
+//const { log } = require("console");
+function numbertype(num)
+{
+//let a=num;
+if (num>0) 
+    {
+        console.log("The given Number is positive");  
+} 
+else if (num<0)
+{
+    console.log("The given Number is negative");
+    
+}
+else
+{
+
+console.log("The given Number is zero");
+
+}
+
+}
+numbertype(2);
